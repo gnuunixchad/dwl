d033a2062aff125c25cf37518e4c4ae751120e89
nate zhou <gnuunixchad@outlook.com>
2025-07-09 18:44:23 +0800

    Mod(config.def.h): Change colors for bar

--- config.def.h
+++ config.def.h
@@ -9,14 +9,14 @@ static const int bypass_surface_visibility = 0;  /* 1 means idle inhibitors will
 static const unsigned int borderpx         = 1;  /* border pixel of windows */
 static const int showbar                   = 1; /* 0 means no bar */
 static const int topbar                    = 1; /* 0 means bottom bar */
-static const char *fonts[]                 = {"monospace:size=10"};
+static const char *fonts[]                 = {"SourceCodePro:size=13"};
 static const float rootcolor[]             = COLOR(0x000000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1f, 0.1f, 0.1f, 1.0f}; /* You can also use glsl colors */
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
-	[SchemeNorm] = { 0xbbbbbbff, 0x222222ff, 0x444444ff },
-	[SchemeSel]  = { 0xeeeeeeff, 0x005577ff, 0x005577ff },
+	[SchemeNorm] = { 0xbbbbbbff, 0x000000ff, 0x000000ff },
+	[SchemeSel]  = { 0xeeeeeeff, 0x427b58ff, 0x427b58ff },
 	[SchemeUrg]  = { 0,          0,          0x770000ff },
 };
 
