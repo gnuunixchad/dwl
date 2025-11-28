6ef72c35d4d925a04f5fc09ddd6f827337af9aa3
nate zhou <gnuunixchad@outlook.com>
2025-11-28 12:18:26 +0800

    Feat(config.def.h): Toggle wshowkeys with `wsk`

--- config.def.h
+++ config.def.h
@@ -191,6 +191,7 @@ static const Key keys[] = {
 	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_space,      spawn,          SHCMD("mpc --toggle && ${HOME}/.local/bin/lsmus") },
 	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_p,          spawn,          SHCMD("mpc prev") },
 	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_n,          spawn,          SHCMD("mpc next") },
+    { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_S,          spawn,          SHCMD("${HOME}/.local/bin/wsk") },
     { MODKEY,                    XKB_KEY_backslash,  spawn,          SHCMD("wlrctl pointer move 9999 9999") },
 	{ MODKEY,                    XKB_KEY_b,          togglebar,      {0} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
