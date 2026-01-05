cd734c1395f5ff1d02f3ffc609ca599575e6fb06
nate zhou <gnuunixchad@outlook.com>
2026-01-05 22:08:59 +0800

    Feat(config.def.h): Append bindings for dunstctl
    `<Mod-Control-Shift-M>` to close all displayed notifications.

--- config.def.h
+++ config.def.h
@@ -192,6 +192,7 @@ static const Key keys[] = {
 	{ WLR_MODIFIER_CTRL,         XKB_KEY_space,      spawn,          SHCMD("fcitx5-remote -t") },
 	{ MODKEY,                    XKB_KEY_n,          spawn,          SHCMD("dunstctl history-pop") },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_N,          spawn,          SHCMD("dunstctl close") },
+	{ MODKEY|WLR_MODIFIER_CTRL|WLR_MODIFIER_SHIFT,XKB_KEY_N,spawn,   SHCMD("dunstctl close-all") },
 	{ MODKEY,                    XKB_KEY_g,          spawn,          SHCMD("${HOME}/.local/bin/shoot") },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_G,          spawn,          SHCMD("${HOME}/.local/bin/shoot --geo") },
 	{ MODKEY|WLR_MODIFIER_CTRL,  XKB_KEY_g,          spawn,          SHCMD("${HOME}/.local/bin/shoot --all") },
